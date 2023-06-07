03a827607aefdd2d0288b92971a909c7b50a7299
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-06-07 15:34:13 -0600

    new app

---
+++ screen/ManageExpense.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function ManageExpense() {
+  return (
+    <View>
+      <Text>ManageExpense</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
